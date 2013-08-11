73b22c184d3b411f48f606db0128003d627ed4fc
Daniel James <daniel@thzinc.com>
2013-08-11 14:50:13 -0700

    Added clarifying comments on authorization details

--- Intake.Web/View/Authentication.cs
+++ Intake.Web/View/Authentication.cs
@@ -16,8 +16,16 @@ namespace Intake.Web.View
 		/// </summary>
 		/// <returns>The authentication cookie value</returns>
 		/// <param name="handle">Handle</param>
+		/// <remarks>
+		/// The "encrypted" cookie value is essentially a difficult-to-reproduce signature of the
+		/// <see cref="Core.Model.User.Handle"/>. This signature is prepended to the handle so that
+		/// the <see cref="DecryptAuthenticationCookieValue()"/> method can take the handle, recompute
+		/// the signature, and compare the given signature to the recomputed signature.
+		/// </remarks>
 		public static string EncryptAuthenticationCookieValue(string handle)
 		{
+			// Reusing GetPasswordDigest, but passing in an empty string for the password plaintext.
+			// This returns a digest that essentially is a combination of the handle and the salt.
 			return string.Concat(Core.Process.User.GetPasswordDigest(handle, string.Empty), ":", handle);
 		}
 
@@ -30,12 +38,18 @@ namespace Intake.Web.View
 		{
 			string handle = null;
 
+			// The expected format of ciphertext is "digest:handle"
 			var atoms = ciphertext.Split(new char[] { ':' }, 2);
 			if (atoms.Length == 2)
 			{
+				// Handle reported in the ciphertext
 				var reportedHandle = atoms[1];
+
+				// Recomputed digest based on the reported handle
 				var digest = EncryptAuthenticationCookieValue(reportedHandle);
 
+				// If the original ciphertext matches the recomputed digest, the reported handle is the handle of the current user
+				// (as opposed to a handle that an attacker might have tried to guess by modifying the ciphertext)
 				if (ciphertext.Equals(digest))
 				{
 					handle = reportedHandle;
@@ -91,7 +105,6 @@ namespace Intake.Web.View
 		{
 			var handle = context.Request.Form["handle"];
 			var passwordPlaintext = context.Request.Form["password"];
-			var salt = ConfigurationManager.AppSettings["Authentication.Salt"];
 			var passwordDigest = Core.Process.User.GetPasswordDigest(handle, passwordPlaintext);
 
 			var user = Core.Model.Factory.UserFactory.Current.GetUser(handle, passwordDigest);
@@ -106,6 +119,8 @@ namespace Intake.Web.View
 			else
 			{
 				context.Items.Add("Authentication.Error", "INVALID_HANDLE_OR_PASSWORD");
+
+				ClearAuthenticationCookie(context);
 
 				handler = new ServerTransferHandler("~/View/Pages/Login.aspx");
 			}
